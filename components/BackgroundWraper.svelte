9eb059e69976b320a2507199493a22b2c88567d5
Mariusz <mmicustommaps@gmail.com>
2025-03-19 18:06:56 +0100

    feat: add background wraper

---
+++ components/BackgroundWraper.svelte
@@ -0,0 +1,20 @@
+<script>
+	import BackgroundSVG from '$lib/components/BackgroundSVG.svelte';
+</script>
+
+<div class="background-wrapper">
+	<span>Test</span>
+	<BackgroundSVG />
+</div>
+
+<style>
+	.background-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		overflow: hidden;
+		z-index: 100;
+	}
+</style>
